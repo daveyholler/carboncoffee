bcfd0d0779f87eccc2d9755909b657bc36fa069f
Davey Holler <daveyholler@hey.com>
2022-12-13 08:15:17 -0800

    First go of it.

---
+++ lua/catppuccin/palettes/carbon.lua
@@ -0,0 +1,37 @@
+-- NOTE: references for Catppuccin Mocha
+-- monochromatic: https://coolors.co/c6d0f5-aeb7d9-969dbc-7e84a0-666a83-4e5167-36374a-1e1e2e-12121c-07070a
+-- analogous 1: https://coolors.co/f5e0dc-f2cdcd-f5c2e7-cba6f7-eba0ac-f38ba8-fab387
+-- analogous 2: https://coolors.co/bfb7e4-8caaee-99d1db-e5c890-85c1dc-a6d189-81c8be
+
+local color_palette = {
+	rosewater = "#8a3ffc",
+	flamingo = "#6929c4",
+	pink = "#be95ff",
+	mauve = "#fa4d56",
+	red = "#0f62fe",
+	maroon = "#fa4d56",
+	peach = "#ffb3b8",
+	yellow = "#f1c21b",
+	green = "#08bdba",
+	teal = "#9ef0f0",
+	sky = "#33b1ff",
+	sapphire = "#0072c3",
+	blue = "#6929c4",
+	lavender = "#3ddbd9",
+
+	text = "#edf5ff",
+	subtext1 = "#d0e2ff",
+	subtext0 = "#e8daff",
+	overlay2 = "#9a9a9a",
+	overlay1 = "#7a7a7a",
+	overlay0 = "#6a6a6a",
+	surface2 = "#555555",
+	surface1 = "#444444",
+	surface0 = "#313131",
+
+	base = "#222222",
+	mantle = "#111111",
+	crust = "#000000",
+}
+
+return color_palette
